fa0cad23da4576e8939e5d312a95e9dcc1114416
saifmkt <139686668+saifmkt@users.noreply.github.com>
2026-09-15 07:58:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function showGame() {
+    document.getElementById("content").innerHTML = `
+        <h3>GAME</h3>
+        <p>
+            Selamat datang di Game Portal.
+            Pilih game favorit kamu dan mulai bermain.
+        </p>
+    `;
+}
+
+function showInfo() {
+    document.getElementById("content").innerHTML = `
+        <h3>INFO</h3>
+        <p>
+            Portal game online dan informasi terbaru.
+            Nikmati pengalaman bermain bersama kami.
+        </p>
+    `;
+}
